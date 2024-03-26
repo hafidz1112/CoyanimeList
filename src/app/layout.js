1b53a66e514c9041732626e6c8e1ff92afd40432
Nama Anda <emailAnda@example.com>
2024-03-26 11:48:46 +0700

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Cuy Anime List',
+  description: 'website anime hafidz',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}suppressHydrationWarning={true}>
+      <Navbar />
+      {children}
+      </body>
+    </html>
+  )
+}
