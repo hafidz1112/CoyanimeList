4f04c95a318be7bc0fa8a1d419135c9b214ab572
Blxckmage <fazadoang15@gmail.com>
2024-03-26 12:06:51 +0700

    fix: search page

---
+++ src/app/search/[keyword]/page.js
@@ -0,0 +1,26 @@
+import AnimeList from "@/components/AnimeList";
+import Header from "@/components/AnimeList/Header";
+
+const Page = async ({ params }) => {
+  const { keyword } = params;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`
+  );
+  const searchAnime = await response.json();
+
+  return (
+    <>
+      <section>
+        <Header
+          title={`pencarian untuk ${keyword}...`}
+          linkTitle="Lihat Semua"
+          linkHref="/populer"
+        />
+        <AnimeList api={searchAnime} />
+      </section>
+    </>
+  );
+};
+
+export default Page;
+
--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -1,31 +1,37 @@
-"use client"
-import { MagnifyingGlass } from "@phosphor-icons/react"
-import { useRouter } from "next/navigation"
-import { useRef } from "react"
-
+"use client";
+import { MagnifyingGlass } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
 
 const InputSearch = () => {
-    const searchRef = useRef()
-    const router = useRouter()
+  const searchRef = useRef();
+  const router = useRouter();
 
-    const handleSearch = (event) => {
-        event.preventDefault()
-        const keyword = searchRef.current.value
+  const handleSearch = (event) => {
+    const keyword = searchRef.current.value;
 
-        router.push(`/search/ ${keyword}`)
-        }
+    if (!keyword || keyword.trim() === "") return;
 
-    return (
-        <div className="relative">
-            <input placeholder="cari anime..."
-                className="w-full p-2 rounded"
-                ref={searchRef}
-            />
-            <button className="absolute top-2 end-2" onClick={handleSearch}>
-                <MagnifyingGlass size={24} />
-            </button>
-        </div>
-    )
-}
+    if (event.key === "Enter" || event.type === "click") {
+      event.preventDefault();
+      router.push(`/search/${keyword}`);
+    }
+  };
 
-export default InputSearch
+  return (
+    <div className="relative">
+      <input
+        placeholder="cari anime..."
+        className="w-full p-2 rounded"
+        ref={searchRef}
+        onKeyDown={handleSearch}
+      />
+      <button className="absolute top-2 end-2" onClick={handleSearch}>
+        <MagnifyingGlass size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default InputSearch;
+
